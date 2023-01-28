f4dc5bfad2cf3447f3d6b8b5d2307dc2829fbc28
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2023-01-27 23:18:00 -0600

    terms of service

--- src/pages/TermsOfServicePage.vue
+++ src/pages/TermsOfServicePage.vue
@@ -6,6 +6,41 @@
   <v-container class="fill-height">
     <v-responsive>
       <h1>Terms of Service</h1>
+      <p>
+        Terms of service for the Join Sound Johnson bot (henceforth "Bot"),
+        this guides our users on how they can use the Bot.
+      </p>
+      <h2>Conditions</h2>
+      <p>General conditions regarding the Bot are under this topic.</p>
+      <v-list>
+        <v-list-item>
+          Our terms and conditions apply to each and every user of the Bot.
+        </v-list-item>
+        <v-list-item>
+          By adding the Bot to your server, you agree to these terms
+          of service and the future terms which we may add after a notice.
+        </v-list-item>
+      </v-list>
+      <h2>Terms of Use</h2>
+      <v-list>
+        <v-list-item>
+          Intentional command spam or attempts to crash the bot should not be made.
+        </v-list-item>
+        <v-list-item>
+          The Bot should not be used in bot spam servers which cause command spam.
+        </v-list-item>
+        <v-list-item>
+          Joshua Smallwood (henceforth "The Developer") reserves the
+          rights to prohibit any server or user from using the Bot.
+        </v-list-item>
+        <v-list-item>
+          The user is responsible for any violation caused by them.
+        </v-list-item>
+        <v-list-item>
+          The Developer has the rights to update terms of service
+          anytime with a notice in the support server.
+        </v-list-item>
+      </v-list>
     </v-responsive>
   </v-container>
 </template>
