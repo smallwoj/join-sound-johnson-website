17dcf0c074877a2ae15584ed5ce7b38d272058be
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2024-05-15 14:58:24 -0500

    use webhistory over webhashhistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import routes from '@/router/routes';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
